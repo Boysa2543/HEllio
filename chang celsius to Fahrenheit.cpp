ae35f82fedffdb56297dd71aefacc300821c13a4
Boysa2543 <52068899+Boysa2543@users.noreply.github.com>
2019-07-05 17:06:18 +0700

    Update chang celsius to Fahrenheit.cpp

--- chang celsius to Fahrenheit.cpp
+++ chang celsius to Fahrenheit.cpp
@@ -7,7 +7,7 @@ void main()
 cout << "Enter Celsius temperature: ";
 	cin >> c;
 	f = c * 9 / 5 + 32;
-	cout  <<"\n"<<"Temperature at this time ::"<< c << " Celsius : " << " to " << f<< " Fahrenheit :";
-	cout << ((f>70) == 0 ? " Now weather in Thailand is COOL" : " Now weather in Thailand is HOT\n") << endl;
+	cout  <<"\n"<<"Temperature at this time :"<< "\n"<< " Celsius : " <<c<<"\n"<< " Fahrenheit :"<<f;
+	cout <<"\n"<< ((f>70) == 0 ? " Now weather in Thailand is COOL" : " Now weather in Thailand is HOT\n") << endl;
 	
 }
