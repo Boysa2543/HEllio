7ed8d2dea4ad37534c1ae8bb14cf243925f33fcb
Boysa2543 <52068899+Boysa2543@users.noreply.github.com>
2019-07-16 10:41:19 +0700

    Update grade.cpp

--- grade.cpp
+++ grade.cpp
@@ -9,5 +9,5 @@ if(grade>=0&&grade<=59)cout<<"You Grade : F\n";
 else if(grade>=60&&grade<=69)cout<<"You Grade : D\n";
 else if(grade>=70&&grade<=79)cout<<"You Grade : C\n";
 else if(grade>=80&&grade<=89)cout<<"You Grade : B\n";
-else if(grade>=90&&grade<=100)cout<<"You Grade : A\n";
+else {cout<<"You Grade : A\n";}
 }
