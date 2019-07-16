d5a86de3271a78f3a5966ad0ddbf4b7d972ab289
Boysa2543 <52068899+Boysa2543@users.noreply.github.com>
2019-07-16 10:35:36 +0700

    Create grade.cpp

---
+++ grade.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+float grade;
+cout<<"Enter your Grade:";
+cin>>grade;
+if(grade>=0&&grade<=59)cout<<"You Grade : F\n";
+else if(grade>=60&&grade<=69)cout<<"You Grade : D\n";
+else if(grade>=70&&grade<=79)cout<<"You Grade : C\n";
+else if(grade>=80&&grade<=89)cout<<"You Grade : B\n";
+else if(grade>=90&&grade<=100)cout<<"You Grade : A\n";
+}
